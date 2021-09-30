5821e8febab570067ed6e5ba021c3e5f0a00fc61
Ronak Mutha <mutharonak97@gmail.com>
2021-09-30 07:48:13 +0530

    fix - added alert for error response

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const btnTranslate = document.querySelector('#btn-translate');
 const translateInput = document.querySelector('#translate-input');
+const alertContainer = document.querySelector('.alert');
+const btnClose = document.querySelector('.close-btn');
+const msg = document.querySelector('.msg');
 const translateOutput = document.querySelector('#translate-output');
 
 const url = "https://api.funtranslations.com/translate/minion.json";
@@ -9,22 +12,45 @@ function constructURL(inputText) {
     return `${url}?text=${encodedURI}`;
 }
 
+function showAlert() {
+    alertContainer.classList.add('show');
+    alertContainer.classList.remove('hide');
+    alertContainer.classList.add('showAlert');
+    setTimeout(function () {
+        closeAlert();
+    }, 4000);
+};
+
+function closeAlert() {
+    alertContainer.classList.remove('show');
+    alertContainer.classList.add('hide');
+}
+
+function fetchingDataFromURL(url) {
+    return fetch(url).then(response => {
+            const json = response.json();
+            return json;
+        })
+
+}
+
 function clickEventHandler(event) {
+    translateOutput.innerText = '';
     console.log("button clicked");
     const translateInputValue = translateInput.value;
     const finalURL = constructURL(translateInputValue);
 
-    fetch(finalURL)
-    .then(response => {
-        var json = response.json();
-        return json;
-    })
+    fetchingDataFromURL(finalURL)
     .then(json => {
-        translateOutput.innerText = json.contents.translated;
+        if(json.success) {
+            translateOutput.innerText = json.contents.translated;
+        } else {
+            showAlert();
+            msg.innerText = `Error: ${json.error.code} - ${json.error.message}`;
+        }
     })
-    .catch((err) => alert("Some error occured, Try again later!"));
-
-
 };
 
 btnTranslate.addEventListener('click', clickEventHandler);
+
+btnClose.addEventListener('click', closeAlert);
